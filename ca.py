fa0b6b5be6abebad1d6a5648baef96e95371021e
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-02-27 23:54:17 +0700

    mai wai lel

--- ca.py
+++ ca.py
@@ -1,2 +1,16 @@
-from pycaret.datasets import get_data
-data = get_data('airbin')
+from flask import Flask, render_template
+import pandas as pd
+import json
+
+app = Flask(__name__)
+
+@app.route('/p')
+def index():
+    data = pd.read_csv('Clean_data.csv')
+    data["DATETIMEDATA"] = pd.to_datetime(data["DATETIMEDATA"])
+    data.set_index('DATETIMEDATA', inplace=True)
+    data = data.asfreq('H')
+    pm25_data = data[['PM25']].to_json(orient='split')
+
+if __name__ == '__main__':
+    app.run(debug=True)
