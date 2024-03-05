5e1d44dc9378858d370dd2aa559c7c7c71ede487
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-03-05 19:42:09 +0700

    testing dash

--- desh.py
+++ desh.py
@@ -4,16 +4,16 @@ import pandas as pd
 import plotly.express as px
 
 # Incorporate data
-df = pd.read_csv('https://github.com/Thanathat26/Pm2.5/blob/main/P_MLcsv')
+df = pd.read_csv('P_ML.csv')
 
 # Initialize the app
 app = Dash(__name__)
 
 # App layout
 app.layout = html.Div([
-    html.Div(children='Testing dash ML'),
+    html.Div(children='My First App with Data, Graph, and Controls'),
     html.Hr(),
-    dcc.RadioItems(options=['PM25', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
+    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
     dash_table.DataTable(data=df.to_dict('records'), page_size=6),
     dcc.Graph(figure={}, id='controls-and-graph')
 ])
@@ -22,7 +22,7 @@ app.layout = html.Div([
 @callback(
     Output(component_id='controls-and-graph', component_property='figure'),
     Input(component_id='controls-and-radio-item', component_property='value')
-)   
+)
 def update_graph(col_chosen):
     fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
     return fig
