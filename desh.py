20f31d09e4edff6e9735570c9349af0ef7ae2860
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-03-05 19:55:49 +0700

    finalyyyyy

--- desh.py
+++ desh.py
@@ -1,5 +1,5 @@
 # Import packages
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
+from dash import Dash, html, dash_table, dcc
 import pandas as pd
 import plotly.express as px
 
@@ -11,21 +11,11 @@ app = Dash(__name__)
 
 # App layout
 app.layout = html.Div([
-    html.Div(children='My First App with Data, Graph, and Controls'),
-    html.Hr(),
-    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
-    dcc.Graph(figure={}, id='controls-and-graph')
-])
-
-# Add controls to build the interaction
-@callback(
-    Output(component_id='controls-and-graph', component_property='figure'),
-    Input(component_id='controls-and-radio-item', component_property='value')
-)
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
-    return fig
+    html.Div(children='My First App with Data and a Graph'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+    dcc.Graph(figure=px.line(df, x='DATETIMEDATA', y='O3')),
+    dcc.Graph(figure=px.line(df, x='DATETIMEDATA', y='WS')),
+    dcc.Graph(figure=px.scatter(df, x='DATETIMEDATA', y='prediction_label', color='O3'))])
 
 # Run the app
 if __name__ == '__main__':
