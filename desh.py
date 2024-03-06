9601d7ed871fef1923f42495a8eb5d377af04d9b
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-03-06 20:36:43 +0700

    bootrap test

--- desh.py
+++ desh.py
@@ -1,28 +1,27 @@
-# Import packages
-from dash import Dash, html, dash_table, dcc,callback,Output,Input
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
 import pandas as pd
 import plotly.express as px
 
 # Incorporate data
-df = pd.read_csv('P_ML.csv')
+df = pd.read_csv('P_ML2.csv')
 
 # Initialize the app
-app = Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # App layout
-app.layout = html.Div([
-    html.Div(children='Predict of PM2.5'),
-    html.Hr(),
-    #dcc.RadioItems(options=['O3', 'WS', 'prediction_label'], value='prediction_label', id='controls-and-radio-item'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10),    
-    #dcc.Graph(figure=px.line(df, x='DATETIMEDATA', y='O3')),
-    #dcc.Graph(figure=px.line(df, x='DATETIMEDATA', y='WS')),
-    dcc.Graph(figure=px.scatter(df, x='DATETIMEDATA', y='prediction_label', color='O3'))])
-
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='DATETIMEDATA', y=col_chosen, histfunc='avg')
-    return fig
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            html.H1("Predict PM2.5"),
+            html.Hr(),
+            dcc.Graph(id='graph', figure=px.scatter(df, x='DATETIMEDATA', y='prediction_label'))
+        ])
+    ])
+])
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)
