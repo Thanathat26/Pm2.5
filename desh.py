9d6559a82a6cb903540696176162fcb09250b4ef
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-03-06 21:18:58 +0700

    prediction

--- desh.py
+++ desh.py
@@ -1,10 +1,10 @@
-import dash 
+import pandas as pd
+import plotly.express as px
+import dash
 from dash import Dash, html, dash_table, dcc
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
-import plotly.express as px
 
 # Incorporate data
 df = pd.read_csv('P_ML2.csv')
@@ -16,13 +16,19 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([
-            html.H1("Predict PM2.5"),
+            html.H1("Predict PM2.5", className="text-center"),
             dbc.Col([html.Hr()]),
             dcc.Graph(id='graph', figure=px.line(df, x='DATETIMEDATA', y='prediction_label', title='PM2.5 Values')),
+        ])
+    ], className="mt-4"),
+    dbc.Row([
+        dbc.Col([
+            html.H2("Predictions", className="text-center"),
             dash_table.DataTable(data=df.to_dict('records'), page_size=6),
         ])
-    ])
+    ], className="mt-4")
 ])
+
 # Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
