fb0eb42e850b74768f70573bef4247c867d9bd67
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-03-06 09:17:20 +0700

    3 option

--- desh.py
+++ desh.py
@@ -1,22 +1,45 @@
 # Import packages
-from dash import Dash, html, dash_table, dcc
+from dash import Dash, html, dash_table, dcc,callback,Output,Input
 import pandas as pd
 import plotly.express as px
 
 # Incorporate data
 df = pd.read_csv('P_ML.csv')
-
+radio_options = [
+    {'label': 'Ozone (O3)', 'value': 'O3'},
+    {'label': 'Wind Speed (WS)', 'value': 'WS'},
+    {'label': 'Predicted PM2.5', 'value': 'prediction_label'}
+]
 # Initialize the app
 app = Dash(__name__)
 
 # App layout
 app.layout = html.Div([
-    html.Div(children='Predict of PM2.5'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
-    dcc.Graph(figure=px.line(df, x='DATETIMEDATA', y='O3')),
-    dcc.Graph(figure=px.line(df, x='DATETIMEDATA', y='WS')),
-    dcc.Graph(figure=px.scatter(df, x='DATETIMEDATA', y='prediction_label', color='O3'))])
+    html.Div(children='PM2.5 Prediction Dashboard'), 
+    html.Hr(),
+    dcc.RadioItems(
+        id='controls-and-radio-item',
+        options=radio_options,
+        value='prediction_label',
+        labelStyle={'display': 'inline-block'}  
+    ),
+    dash_table.DataTable(
+        data=df.to_dict('records'),
+        page_size=10
+    ),
+    dcc.Graph(id='selected-column-graph') 
+])
+@callback(
+    Output(component_id='selected-column-graph', component_property='figure'),
+    Input(component_id='controls-and-radio-item', component_property='value')
+)
+def update_graph(selected_column):
+    if selected_column in ['O3', 'WS','prediction_label']:
+        fig = px.histogram(df, x='DATETIMEDATA', y=selected_column, histfunc='avg', color='')
+    else: 
+        fig = px.histogram(df, x='DATETIMEDATA', y=selected_column)
 
+    return fig
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
