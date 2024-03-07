ebb6056b9c9ea2d91a7213570f7f62d25402c731
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-03-07 08:44:09 +0700

    graph type

--- desh.py
+++ desh.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import dash
-from dash import Dash, html, dash_table, dcc
+from dash import Dash, html, dash_table, dcc, Input, Output
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
@@ -12,23 +12,50 @@ df = pd.read_csv('P_ML2.csv')
 # Initialize the app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+# Define graph types
+graph_types = {"Line Chart": px.line, "Scatter Plot": px.scatter, "Bar Plot": px.bar, "Histogram": px.histogram}
+
 # App layout
 app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([
             html.H1("Predict PM2.5", className="text-center"),
-            dbc.Col([html.Hr()]),
-            dcc.Graph(id='graph', figure=px.line(df, x='DATETIMEDATA', y='prediction_label', title='PM2.5 Values')),
+            dbc.Col([
+                html.Hr(),
+                html.Label("Select type of graph:"),
+                dcc.Dropdown(
+                    id="graph-type",
+                    options=[{"label": option, "value": option} for option in graph_types.keys()],
+                    value="Line Chart",
+                ),
+                html.Button("Update Graph", id="update-button", n_clicks=0),
+            ])
+        ], width=6),
+        dbc.Col([
+            dcc.Graph(id="graph")
         ])
     ], className="mt-4"),
     dbc.Row([
         dbc.Col([
             html.H2("Predictions", className="text-center"),
-            dash_table.DataTable(data=df.to_dict('records'), page_size=6),
+            dash_table.DataTable(
+                id="predictions-table", data=df.to_dict("records"), page_size=6
+            ),
         ])
     ], className="mt-4")
 ])
 
+
+@app.callback(
+    [Output("graph", "figure")],
+    [Input("update-button", "n_clicks"), Input("graph-type", "value")],
+)
+def update_graph(n_clicks, graph_type):
+    if n_clicks > 0:
+        return [graph_types[graph_type](df, x="DATETIMEDATA", y="prediction_label", title="PM2.5 Values")]
+    else:
+        return [] 
+
 # Run the app
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
