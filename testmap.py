106c216417cb26e1f74ee0bc37100c9ec20c71b6
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-03-13 13:20:34 +0700

    color change bug

--- testmap.py
+++ testmap.py
@@ -3,12 +3,13 @@ import pandas as pd
 import geopandas as gpd
 gapminder = pd.read_csv('P_MLMap.csv')
 print(gapminder.head(15))
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-gapminder_map = gapminder.merge(world, left_on='iso_alpha', right_on='iso_a3', how='left')
-fig = px.choropleth(gapminder_map,
+gapminder['DATETIMEDATA'] = pd.to_datetime(gapminder['DATETIMEDATA'])
+print(gapminder.head(15))
+fig = px.choropleth(gapminder,
                     locations="iso_alpha",
                     color="prediction_label",
-                    scope="world", 
+                    scope="asia", 
                     animation_frame="DATETIMEDATA",
                    )
+
 fig.show()
